93b2cdc8c5ed02778f7f9946b5907e08b80b84f9
henkkapoika <128515915+henkkapoika@users.noreply.github.com>
2023-03-27 19:51:16 +0300

    Harjoituksia2

---
+++ Keskiarvo/Keskiarvo/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Keskiarvo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string input;
+            double firstNumber;
+            double secondNumber;
+            double thirdNumber;
+            double averageNumber;
+            double keskiArvo;
+            Console.Write("Anna ensimmäinen luku: ");
+            input = Console.ReadLine();
+            firstNumber = double.Parse(input);
+
+            Console.Write("Anna toinen luku: ");
+            input = Console.ReadLine();
+            secondNumber = double.Parse(input);
+
+            Console.Write("Anna kolmas luku: ");
+            input = Console.ReadLine();
+            thirdNumber = double.Parse(input);
+
+            // Laskukaavio.
+
+            averageNumber = firstNumber + secondNumber + thirdNumber;
+            keskiArvo = averageNumber / 3;
+
+            Console.WriteLine($"Keskiarvo on: {keskiArvo}");
+
+                
+               
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Kolmion pinta-ala/Kolmion pinta-ala/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kolmion_pinta_ala
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string input;
+            double Kanta;
+            double Korkeus;
+            double Triangle;
+            Console.Write("Anna kanta: ");
+            input = Console.ReadLine();
+            Kanta = double.Parse(input);
+
+            Console.Write("Anna korkeus: ");
+            input = Console.ReadLine();
+            Korkeus = double.Parse(input);
+
+            // Laskukaavio.
+
+            Triangle = (Kanta * Korkeus) / 2;
+            Console.WriteLine($"Kolmion pinta-ala on: {Triangle}");
+
+            Console.ReadKey();
+
+
+
+
+
+
+        }
+    }
+}
