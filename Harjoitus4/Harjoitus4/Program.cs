0bea2c00c8bcaa85c03fd3f8248ee4b314cbe90a
henkkapoika <128515915+henkkapoika@users.noreply.github.com>
2023-03-27 18:58:24 +0300

    Harjoitustehtäviä

---
+++ Harjoitus4/Harjoitus4/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harjoitus4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Palkan lasku 27.3.
+            
+            // Muuttujat
+            string input; // Tallenetaan käyttäjän arvot
+
+            // Tuntipalkka
+            double hourlySalary;
+
+            // Tunnit, joista maksetaan 100% palkka
+            double normalHours;
+
+            // Tunnit, joista maksetaan 150% palkka
+            double plus50PercentHours;
+
+            // Tunnit, joista maksetaan 200% palkka
+            double plus100PercentHours;
+
+            // Laskettu kuukausipalkka
+            double totalSalary;
+
+            Console.Write("Anna tuntipalkka: ");
+            input = Console.ReadLine();
+            hourlySalary = double.Parse(input);
+
+            Console.Write("Anna normaalit tunnit: ");
+            // Käyttäjän arvon lukeminen ilman -input- muuttujan käyttöä
+            normalHours = double.Parse(Console.ReadLine());
+
+            Console.Write("Anna 50% korotetut tunnit: ");
+            plus50PercentHours = double.Parse(Console.ReadLine());
+
+            Console.Write("Anna 100% korotetut tunnit: ");
+            input = Console.ReadLine();
+            plus100PercentHours = double.Parse(input);
+
+            // Laskuoperaatio
+            // Laskukaavassa arvot 1.5 ja 2 käytetään termiä "Magic Number"
+            // ratkaisu olisi tallentaa arvot sopivan nimisiin muuttujiin.
+            // Erityisesti jos arvoa käytetään useaan kertaan.
+            totalSalary = hourlySalary * normalHours + // laskuoperaation voi jakaa usealle riville
+                        hourlySalary * 1.5 * plus50PercentHours +
+                        hourlySalary * 2 * plus100PercentHours;
+
+            Console.WriteLine($"Kuukausi palkka on {totalSalary}");
+
+            Console.ReadKey();
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Opetuspaiva2/Opetuspaiva2/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opetuspaiva2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Harjoitus 22.3.
+
+            int age = 27; // Integer - kokonaisluku
+            age = 28; // Muokataan muuttujan arvo
+            // " shift+2
+            // { = ctrl + alt + 7
+            // } = ctrl + alt + 0
+            // $ = ctrl + alt + 4
+            Console.WriteLine($"{age} on käyttäjän ikä");
+
+            
+
+            double value1 = 0.1;
+            double value2 = 0.2;
+            double value3 = value1 + value2;
+            Console.WriteLine(value3);
+
+            double height = 1.78; // Double - liukuluku(desimaalit)
+            height = 1.8;
+            Console.WriteLine($"{height} on käyttäjän pituus");
+
+            string name = "Henrik";
+            name = "Pekka";
+            Console.WriteLine($"{name} käyttäjän nimi.");
+
+            bool loggedIn = true; // boolean - totuusarvomuuttuja
+            loggedIn = false;
+            Console.WriteLine($"Käyttäjä on kirjautunut: {loggedIn}");
+
+            // ' -> Ä oikealla puolella
+            char replaceWith = 't'; // char, Yksi merkki
+            replaceWith = 'T';
+            Console.WriteLine($"Korvataan kirjaimella {replaceWith}");
+
+            // Harjoitus 23.3.
+
+            string userInput; // Tallenetaan käyttäjän syöttämä arvo
+            Console.WriteLine();
+            Console.WriteLine("Harjoitus 23.3. ");
+           
+            Console.Write("Anna pituus metreinä: ");
+            userInput = Console.ReadLine(); // Käyttäjän syöttämä luku tallessa
+            double length = double.Parse(userInput); // Muokataan string arvo double dataksi
+
+            Console.Write("Anna leveys metreinä: ");
+            userInput = Console.ReadLine();
+            double width = double.Parse(userInput);
+
+            double area = length * width;
+            Console.WriteLine($"Alueen pinta-ala: {area}");
+
+
+
+
+
+            // Koodin lopussa
+            Console.ReadKey();
+        }
+    }
+}
