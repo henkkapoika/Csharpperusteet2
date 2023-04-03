c98aa59cb50193316ee8bc5feb4f5cb1aeb986d7
henkkapoika <henriknerg@hotmail.com>
2023-04-03 09:03:31 +0300

    kotitehtavat

---
+++ Lampotila muunnin/Lampotila muunnin/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lampotila_muunnin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Montako astetta: ");
+            double temperature = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Muutetaanko mutoon Fahrenehit = F vai Celsius = C: ");
+            char character = Convert.ToChar(Console.Read());
+            double endTemperature;
+
+            char[] c_Char = { 'c', 'C' };
+            char[] f_Char = { 'f', 'F' };
+
+            if (character == c_Char[0] || character == c_Char[1])
+            {
+                endTemperature = (temperature - 32) * 5 / 9;
+                Console.WriteLine($"Lämpötila on {endTemperature} celsius astetta.");
+
+            }
+            if (character == f_Char[0] || character == f_Char[1])
+            {
+                endTemperature = temperature * 9 / 5 + 32;
+                Console.WriteLine($"Lämpötila on {endTemperature} Fahrenheit astetta");
+
+            }
+
+            Console.ReadKey();
+
+
+
+
+            
+        }
+    }
+}
---
+++ Parillinen pariton/Parillinen pariton/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parillinen_pariton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Anna luku johon saakka tulostetaan lukuja: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write("Tulostetaanko parilliset (x) vai parittomat (y): ");
+            char character = Convert.ToChar(Console.Read());
+
+            char[] x_Char = { 'x', 'X' };
+            char[] y_Char = { 'y', 'Y' };
+
+
+            
+            
+                if (character == x_Char[0] || character == x_Char[1])
+                {
+                Console.WriteLine("Parilliset :");
+                for (int n = 1; n < (number + 1); n++)
+                    {
+                    if (n % 2 == 0)
+                        {
+                        Console.WriteLine(n);
+                    }
+                    }
+                }
+
+
+
+            if (character == y_Char[0] || character == y_Char[1])
+            {
+                Console.WriteLine("Parittomat :");
+                for (int n = 1; n < (number + 1); n++)
+                {
+                    if (n % 2 != 0)
+                    {
+                        Console.WriteLine(n);
+                    }
+                }
+            }
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+    }   }
