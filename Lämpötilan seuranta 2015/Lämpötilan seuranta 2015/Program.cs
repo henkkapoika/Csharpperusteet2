66541172ea8fd28826b60e7e9c1485413bd23de5
henkkapoika <128515915+henkkapoika@users.noreply.github.com>
2023-04-18 16:20:09 +0300

    harjoituksia

---
+++ Lämpötilan seuranta 2015/Lämpötilan seuranta 2015/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lämpötilan_seuranta_2015
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Taulukon ja listan luonti
+
+            string[] measurementTimes = { "6:00", "12:00", "18:00", "24:00" };
+
+            double[] temperaturesArray = new double[measurementTimes.Length];
+            List<double> temperaturesList = new List<double>();
+
+            // Silmukka
+            for (int i = 0; i < measurementTimes.Length; i++)
+            {
+                Console.Write($"Kello {measurementTimes[i]} lämpötila: ");
+                double input = double.Parse(Console.ReadLine());
+                temperaturesArray[i] = input; // Taulukko
+                temperaturesList.Add(input); // Lista
+            }
+            // Suurin, pienin ja keskilämpötila käyttämällä valmiita metodeja
+            Console.WriteLine($"Taulukon suurin lämpötila on: {temperaturesArray.Max()}");
+            Console.WriteLine($"Taulukon pienin lämpötila on: {temperaturesArray.Min()}");
+            Console.WriteLine($"Taulukon keskilämpötila on: {temperaturesArray.Average()}");
+
+            Console.WriteLine($"Listan suurin lämpötila on: {temperaturesList.Max()}");
+            Console.WriteLine($"Listan pienin lämpötila on: {temperaturesList.Min()}");
+            Console.WriteLine($"Listan keskilämpötila on: {temperaturesList.Average()}");
+
+            
+            // Arvojen etsiminen omalla koodilla
+            // Algoritmi, termi koodille joka suorittaa jonkin asian
+            // Tässä algoritmi, joka laskee keskiarvon
+            int count = 0;
+            double sum = 0;
+
+            foreach (double temp in temperaturesArray)
+            {
+                sum += temp;
+                count++;
+            }
+
+            Console.WriteLine($"Taulukon keskiarvo on {sum/count}");
+
+            // Algoritmi, joka hakee listasta suurimman arvon
+
+            double value = 0; // Tästä arvosta lähdetään liikkeelle ja tallennetaan suurin arvo
+
+            for (int i = 0; i < temperaturesList.Count; i++)
+            {
+                // Korvataan oletusarvo 0, ensimmäisellä kierroksella
+                if (i == 0)
+                {
+                    value = temperaturesList[i];
+                }
+                
+                // Onko uusi arvo suurempi kuin vanha arvo
+                if (temperaturesList[i] > value)
+                {
+                    value = temperaturesList[i]; // Otetaan talteen isompi arvo
+                }
+            }
+
+            Console.WriteLine($"Listan suurin arvo on: {value}");
+
+            // Algoritmi, joka hakee taulukosta pienimmän arvon
+
+            double valuE = 0; // Tästä arvosta lähdetään liikkeelle ja tallennetaan pienin arvo
+
+            for (int i = 0; i < temperaturesArray.Length; i++)
+            {
+                // Korvataan oletusarvo 0, ensimmäisellä kierroksella
+                if (i == 0)
+                {
+                    valuE = temperaturesArray[i];
+                }
+
+                // Onko uusi arvo pienempi kuin vanha arvo
+                if (temperaturesArray[i] < valuE)
+                {
+                    valuE = temperaturesArray[i]; // Otetaan talteen pienempi arvo
+                }
+            }
+
+            Console.WriteLine($"Taulukon pienin arvo on: {valuE}");
+
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Vokaalit2015 2/Vokaalit2015 2/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vokaalit2015_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 34B C# Vokaalit taulukkoon
+            // https://moodle.sakky.fi/mod/assign/view.php?id=1164552
+            #region
+
+            // 1.Lue käyttäjältä sana talteen
+
+            Console.Write("Syötä sana: ");
+            string userInput = Console.ReadLine();
+
+            // 2.Luo taulukko sanan vokaaleille
+
+            // Taulukon pituus täytyy määrittää, sitä luodessa
+            // Luodaan taulukon pituudeksi sama, kuin sanan pituus, silloin ainakin riittää tila
+
+            // datatyyppi [] taulukonNimi = new datatyyppi[taulukon pituus];
+            char[] vocalsInWord = new char[userInput.Length];
+
+            // List<datatyyppi> nimi = new List<datatyyppi>();
+            List<char> vocalsUsingList = new List<char>(); // Lista
+            // Lista on oletuksena tyhjä ja sen pituus/Count on 0
+            // vocalsUsingList[0] = 'n'; // Kaatuu, ArgumentOutOfRangeException, koska lista on tyhjä
+            // Sen elementtejä ei voida vielä käyttää
+
+
+            // 3.Käy sanan merkit läpi silmukassa
+
+            // foreach olisi toinen vaihtoehto, mutta siinä on hankala saada indeksi,
+            // jota tarvitaan taulukossa
+
+            // indeksi (int i = 0) alkaa arvosta 0, koska indeksit alkavat nollasta,
+            //         indeksi muuttujaa käytetään taulukossa
+            // Silmukan ehtona on taulukon pituus / indeksien lukumäärä. Kun iteroitava muuttuja i
+            //          on taulukon indeksien ulkopuolella, silmukka päättyy.
+            //          Esim: "test".Length == 4, jonka indeksit ovat 0, 1, 2, 3. Kun i saapuu arvoon 4,
+            //          eli 4 < 4 ei ole enää totta, silmukka päättyy
+            // Iterointi, i++
+
+            for (int i = 0; i < userInput.Length; i++)
+            {
+                // Kirjoitetaan merkit yksi kerrallaan, merkkiä voi käyttää syntaksilla
+                // muuttujanNimi[indeksi]
+                // Console.WriteLine(userInput[i]);
+
+                // 4.Tallenna merkki taulukkoon, jos se on vokaali.
+                // Lisätään merkin tallennus ehdon sisälle, joka tarkistaa, että merkki on vokaali
+                if (userInput[i] == 'a' || userInput[i] == 'e' || userInput[i] == 'i' ||
+                     userInput[i] == 'o' || userInput[i] == 'u' || userInput[i] == 'y'
+                     || userInput[i] == 'ä' || userInput[i] == 'ö')
+                {
+                    vocalsInWord[i] = userInput[i];
+                    vocalsUsingList.Add(userInput[i]); // Listaan lisätään elementti .Add()-metodilla
+                }
+            }
+
+            // 5.Luo silmukka, jossa tulostetaan vokaalit taulukosta käyttäjälle.
+
+            // for olisi toinen vaihtoehto
+            foreach (char symbol in vocalsInWord)
+            {
+                // Ei tulosteta tyhjiä elementtejä
+                // char taulukon oletusarvo on 0, jos symbol on muutakuin 0 (symbol != 0)
+                // tulostetaan merkki
+                if (symbol != 0)
+                {
+                    Console.WriteLine(symbol);
+                }
+            }
+
+            // Myös foreach toimisi
+            for (int i = 0; i < vocalsUsingList.Count; i++) // Lista
+            {
+                Console.WriteLine("Lista: " + vocalsUsingList[i]);
+            }
+
+            #endregion
+
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+
+        }
+    }
+}
